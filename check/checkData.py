e8b353c57d9f1f12777fd28bf8f9e0d5a6c172b5
shanxuan <781244184@qq.com>
2019-11-21 05:16:54 +0800

    check data

---
+++ check/checkData.py
@@ -0,0 +1,38 @@
+
+
+
+def load_data_dblp(path='/Users/csx/GitProject/HAN/data/acm/ACM3025.mat'):
+    data = sio.loadmat(path)
+    truelabels, truefeatures = data['label'], data['feature'].astype(float)
+    N = truefeatures.shape[0]
+    rownetworks = [data['PAP'] - np.eye(N), data['PLP'] - np.eye(N)]  # , data['PTP'] - np.eye(N)]
+
+    y = truelabels
+    train_idx = data['train_idx']
+    val_idx = data['val_idx']
+    test_idx = data['test_idx']
+
+    train_mask = sample_mask(train_idx, y.shape[0])
+    val_mask = sample_mask(val_idx, y.shape[0])
+    test_mask = sample_mask(test_idx, y.shape[0])
+
+    y_train = np.zeros(y.shape)
+    y_val = np.zeros(y.shape)
+    y_test = np.zeros(y.shape)
+    y_train[train_mask, :] = y[train_mask, :]
+    y_val[val_mask, :] = y[val_mask, :]
+    y_test[test_mask, :] = y[test_mask, :]
+
+    # return selected_idx, selected_idx_2
+    print('y_train:{}, y_val:{}, y_test:{}, train_idx:{}, val_idx:{}, test_idx:{}'.format(y_train.shape,
+                                                                                          y_val.shape,
+                                                                                          y_test.shape,
+                                                                                          train_idx.shape,
+                                                                                          val_idx.shape,
+                                                                                          test_idx.shape))
+    truefeatures_list = [truefeatures, truefeatures, truefeatures]
+    return rownetworks, truefeatures_list, y_train, y_val, y_test, train_mask, val_mask, test_mask
+
+
+if __name__ == '__main__':
+    load_data_dblp()
